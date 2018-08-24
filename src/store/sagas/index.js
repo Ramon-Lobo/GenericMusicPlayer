79d971bea0dd96d46b5b37fa077b085dc593b5f1
Ramon Lobo <ramonlobo@id.uff.br>
2018-08-23 22:00:20 -0300

    Added: data source with json-server

--- src/store/sagas/index.js
+++ src/store/sagas/index.js
@@ -1,5 +1,10 @@
 import { all, takeLatest } from 'redux-saga/effects';
 
+import { Types as AlbumsTypes } from 'store/ducks/albums';
+import { getAlbums } from './albums';
+
 export default function* rootSaga() {
-  return yield true;
+  return yield all([
+    takeLatest(AlbumsTypes.GET_REQUEST, getAlbums),
+  ]);
 }
